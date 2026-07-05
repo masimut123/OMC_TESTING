361d79391347fe4171b5f441ae780d2bde0c1f65
ammr31 <ammarridzuan31@gmail.com>
2026-07-05 12:15:05 +0800

    test to push

--- OMC_TESTING/Form1.cs
+++ OMC_TESTING/Form1.cs
@@ -2,6 +2,8 @@ namespace OMC_TESTING
 {
     public partial class Form1 : Form
     {
+        //testing to oush to github
+        string github = "test";
         public Form1()
         {
             InitializeComponent();
